c113f8b58954d356aa3566b36524644522f27c8b
Eugene <zerom2016romanenko@gmail.com>
2020-12-03 01:53:10 +0200

    update bot with google NL api

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 TOKEN = '1210740794:AAEOjpTgE0z12c6Dx9NEHM0pPnMZ-3OXlQ4'  # bot token
 
 WEB_URL = 'https://schedule-bot-telegram.herokuapp.com/'
+
+API_KEY = "AIzaSyCce0uCHRkQ_K28ePoWPMcD3VbgaXOs73Y"
--- main.py
+++ main.py
@@ -5,79 +5,51 @@ import datetime
 import os
 from flask import Flask, request
 
+from googleapiclient.discovery import build
+
+APIKEY = config.API_KEY
 TOKEN = config.TOKEN
 WEB_URL = config.WEB_URL
 
-open_time = datetime.time(hour=16, minute=55, second=0)
-close_time = open_time.replace(hour=open_time.hour + 5)
-open_time_str = open_time.strftime("%H:%M:%S")
+lang_service = build('language', 'v1beta1', developerKey=APIKEY)
 
 bot = telebot.TeleBot(TOKEN)
-# job = schedule.every().day.at(open_time_str)
-job = schedule.every(1).days
-# job = schedule.every(5).seconds
 is_run = False
-
-is_first_time = True
-
 server = Flask(__name__)
+
+
+def analyze_text(text):
+    response = lang_service.documents().analyzeSyntax(
+        body={
+            'document': {
+                'type': 'PLAIN_TEXT',
+                'content': text
+            }
+        }
+    ).execute()
+
+    tokens = response['tokens']
+    result = "Number of words: " + str(len(tokens)) + "\n"
+    for token in tokens:
+        result += str(token['text']['content']) + " - is " + str(token['partOfSpeech']['tag'])
+        if token['partOfSpeech']['tag'] == 'VERB':
+            result += ". Time of verb is " + str(token['partOfSpeech']['tense'])
+
+        result += "\n"
+
+    return result
 
 
 @bot.message_handler(commands=['help'])
 def help_handle(message):
-    bot.send_message(message.chat.id, "it is a simple bot, that can schedule poll. "
-                                      "Now it can only send poll every day at 15:00 but in future "
-                                      "I am going to add ability to schedule your time and poll")
+    bot.send_message(message.chat.id, "Bot help you to understand feeling of entering fraze")
 
 
 @bot.message_handler(commands=['start'])
 def start_handle(message):
-    global is_run
-    global is_first_time
-    if not is_run:
-        is_run = True
-
-        job.do(bot.send_poll,
-               chat_id=message.chat.id,
-               question="Lets roll today!",
-               options=["20:00", "21:00", "another time", "not today"],
-               is_anonymous=False,
-               allows_multiple_answers=True,
-               close_date=close_time)
-
-        if is_first_time:
-            is_first_time = False
-            bot.send_poll(chat_id=message.chat.id,
-                          question="Lets roll today!",
-                          options=["20:00", "21:00", "another time", "not today"],
-                          is_anonymous=False,
-                          allows_multiple_answers=True,
-                          close_date=close_time)
-
-        while is_run:
-            schedule.run_pending()
-
-    else:
-        bot.send_message(message.chat.id, "Job is already scheduled")
-
-
-@bot.message_handler(commands=['stop'])
-def stop_handle(message):
-    global is_run
-    global is_first_time
-    if is_run:
-        schedule.cancel_job(job)
-        is_run = False
-        is_first_time = True
-
-    else:
-        bot.send_message(message.chat.id, "Job is not scheduled")
-
-
-@bot.message_handler(commands=['status'])
-def status_handle(message):
-    global is_run
-    bot.send_message(message.chat.id, f"Job is schedule: {is_run}")
+    text = message.text
+    result = analyze_text(text)
+    bot.send_message(message.chat.id, result)
 
 
 # bot.polling(none_stop=True)
